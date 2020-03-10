2fe674b39e287cd6b3eb5b90d64b9ef2b971408b
Brad <bjkapteyn@gmail.com>
2020-03-10 14:37:11 -0400

    added nav bar and styling, added routing for nav link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import {VillainsComponent} from './villains/villains.component';
 
+//path is the actual string placed as the URL and component is the component that will be displayed
+//when the string is entered. Ex. www.domain.com/villains will display VillainsComponent.
 const routes: Routes = [
   {path: 'villains', component: VillainsComponent}
 ];
 
 @NgModule({
+
+  //adds the RouterModule to the AppRoutingModule and configures the delclared routes in one step
   imports: [
     RouterModule.forRoot(routes)
   ],
+  //allows the use of RouterModule throughout the app
   exports: [
     RouterModule
   ]
