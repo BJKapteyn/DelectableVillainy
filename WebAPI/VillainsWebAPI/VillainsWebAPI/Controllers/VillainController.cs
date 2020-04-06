c9a67822871693a169d0c81588eb3ef45d4e7034
Brad <bjkapteyn@gmail.com>
2020-04-06 11:11:05 -0400

    added httpget for villain names

--- WebAPI/VillainsWebAPI/VillainsWebAPI/Controllers/VillainController.cs
+++ WebAPI/VillainsWebAPI/VillainsWebAPI/Controllers/VillainController.cs
@@ -1,15 +1,32 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using VillainsWebAPI.Models;
 
 namespace VillainsWebAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class VillainController : ControllerBase
+  [Route("api/[controller]")]
+  [ApiController]
+  public class VillainController : ControllerBase
+  {
+    DelectableVillainyContext VillainDB;
+    public VillainController(DelectableVillainyContext villainDB)
     {
+      VillainDB = villainDB;
     }
+
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+      
+      using (VillainDB)
+      {
+        var villains = VillainDB.Villains.Select(x => x.Name).ToList();
+        return villains;
+      }
+    }
+  }
 }
