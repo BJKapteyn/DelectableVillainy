ed19b031b72656b007909a43b7ec78f7b48a48ef
Brad <bjkapteyn@gmail.com>
2020-05-04 14:10:10 -0400

    re-scaffolded db to add description context

--- WebAPI/VillainsWebAPI/VillainsWebAPI/Models/Villains.cs
+++ WebAPI/VillainsWebAPI/VillainsWebAPI/Models/Villains.cs
@@ -1,28 +1,27 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 
 namespace VillainsWebAPI.Models
 {
-  public partial class Villain
-  {
-    public Villain()
+    public partial class Villains
     {
-      Abilities = new HashSet<Abilities>();
-      BaseStats = new HashSet<BaseStats>();
+        public Villains()
+        {
+            Abilities = new HashSet<Abilities>();
+            BaseStats = new HashSet<BaseStats>();
+        }
+
+        public int VillainId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public short? Age { get; set; }
+        public string Nemesis { get; set; }
+        public string Hometown { get; set; }
+        public string Background { get; set; }
+        public string WeaponOfChoice { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Abilities> Abilities { get; set; }
+        public virtual ICollection<BaseStats> BaseStats { get; set; }
     }
-    //Update DB model WARNING will overwrite Models DO NOT USE AFTER STARTING WORK ON MODELS
-    //Scaffold-DbContext "Server=LAPTOP-T6VP6L6I\SQLEXPRESS;Database=DelectableVillainy;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Force
-
-    public int VillainId { get; set; }
-    public string Name { get; set; }
-    public string Gender { get; set; }
-    public short? Age { get; set; }
-    public string Nemesis { get; set; }
-    public string Hometown { get; set; }
-    public string Background { get; set; }
-    public string WeaponOfChoice { get; set; }
-
-    public virtual ICollection<Abilities> Abilities { get; set; }
-    public virtual ICollection<BaseStats> BaseStats { get; set; }
-  }
 }
